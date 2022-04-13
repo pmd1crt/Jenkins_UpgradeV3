28cdc2c47f0fcea979aacb254f70cd6d42746e6a
Desai, Pankti <panchhi007@yahoo.co.in>
2022-04-13 13:15:21 +0100

    test 12

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,7 +37,7 @@ public class TestGreeter {
     
     @Test
   public void greetShouldIncludeGreetingMessage1() {
-    String someone = "Hello World, Thanks you";
+    String someone = "Hello World, It's me Pankti.Thanks you";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
